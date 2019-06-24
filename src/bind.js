ce07e6f225918cd44107d4729debd1169bd5a390
vpolets <vpolets@gmail.com>
2019-06-24 14:20:48 +0300

    add task and tests

---
+++ src/bind.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * Write a function (factory). It takes a callback as a first param and all
+ * the other params after. It creates a function (device). The callback given
+ * to the factory is called each time you call the device. It receives all the
+ * params from factory (starting from second one) and all the params from the
+ * device.
+ *
+ * For Example:
+ *
+ * const f = (...args) => { console.log(args) };
+ * const device = bind(f, 1, 2, 3);
+ * device(); // 1, 2, 3
+ * device(4, 5, 6); // 1, 2, 3, 4, 5, 6
+ *
+ * @param {Function} callback
+ *
+ * @return {Function}
+ */
+function bind(callback) {
+  // write code here
+}
+
+module.exports = bind;
---
+++ src/bind.test.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const bind = require('./bind');
+
+test('add two numbers 1 + 2 = 3', () => {
+  expect(bind(1, 2))
+    .toBe(3);
+});
+
+describe('Basic scenario', () => {
+  const f = (...args) => args;
+  let device;
+  const argsBase = [1, 2, 3];
+  const argsExtra = [4, 5, 6];
+
+  beforeEach(() => {
+    device = bind(f, ...argsBase);
+  });
+
+  test('should return expected array', () => {
+    expect(device())
+      .toStrictEqual([1, 2, 3]);
+  });
+
+  test('should return expected array', () => {
+    expect(device(...argsExtra))
+      .toStrictEqual([1, 2, 3, 4, 5, 6]);
+  });
+
+  test('should not change base and extra params', () => {
+    expect(argsBase)
+      .toStrictEqual([1, 2, 3]);
+    expect(argsExtra)
+      .toStrictEqual([4, 5, 6]);
+  });
+});
+
+describe('Call bind without args', () => {
+  const f = (...args) => args;
+  let device;
+  const argsExtra = [4, 5, 6];
+
+  beforeEach(() => {
+    device = bind(f);
+  });
+
+  test('should return expected array', () => {
+    expect(device())
+      .toStrictEqual([]);
+  });
+
+  test('should return expected array', () => {
+    expect(device(...argsExtra))
+      .toStrictEqual([4, 5, 6]);
+  });
+
+  test('should not change base and extra params', () => {
+    expect(argsExtra)
+      .toStrictEqual([4, 5, 6]);
+  });
+});
